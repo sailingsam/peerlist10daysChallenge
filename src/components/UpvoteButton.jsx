cb69209767588b2633585c75680a6bcbf71029bb
sailingsam <jainsaksham1004@gmail.com>
2024-07-19 17:03:31 +0530

    deploying 1 draft

--- src/components/UpvoteButton.jsx
+++ src/components/UpvoteButton.jsx
@@ -12,18 +12,22 @@ const UpvoteButton = () => {
     <div className="group p-2 rounded-2xl hover:bg-green-200">
       <div
         onClick={handleUpvote}
-        className={` flex flex-col items-center justify-center w-28 h-28 p-5 border rounded-xl cursor-pointer select-none ${
+        className={`flex flex-col items-center justify-center w-28 h-28 p-5 border rounded-xl cursor-pointer select-none ${
           upvotes === 1 ? "bg-gray-100 border-green-500" : "bg-gray-100 border-gray-600"
         }`}
       >
-        <div className="rounded-full p-1 group-hover:bg-green-200">
+        <div className="rounded-full p-1 group-hover:bg-green-200 transform group-hover:translate-y-[-4px] transition-all duration-300">
           {upvotes === 1 ? (
             <PiArrowFatUpFill className="text-2xl text-green-500" />
           ) : (
             <PiArrowFatUpLight className="text-2xl text-gray-500" />
           )}
         </div>
-        <span className={`mt-2 text-xl font-bold ${upvotes === 1 ? "text-green-500" : "text-gray-700"}`}>
+        <span
+          className={`mt-2 text-xl font-bold ${
+            upvotes === 1 ? "text-green-500 animate-slide-down" : "text-gray-700 animate-slide-up"
+          }`}
+        >
           7{upvotes}
         </span>
       </div>
