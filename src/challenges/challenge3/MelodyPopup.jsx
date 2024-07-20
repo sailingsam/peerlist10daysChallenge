9da368914680c984808962b7a52d2748c179e8a9
sailingsam <jainsaksham1004@gmail.com>
2024-07-21 04:09:46 +0530

    adding animation for c3

--- src/challenges/challenge3/MelodyPopup.jsx
+++ src/challenges/challenge3/MelodyPopup.jsx
@@ -1,12 +1,15 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FaPlay, FaPause } from "react-icons/fa";
 import Audi from "./audio.mp3";
+import { Slider } from "antd";
+import "./MelodySlider.css";
 
 export default function MelodyPopup() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const audio = useRef(new Audio(Audi));
-  
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const togglePlay = () => {
     if (isPlaying) {
@@ -16,22 +19,56 @@ export default function MelodyPopup() {
     }
     setIsPlaying(!isPlaying);
   };
+
+  const onSeek = (value) => {
+    audio.current.currentTime = value;
+    setCurrentPosition(value);
+  };
+
+  useEffect(() => {
+    const setAudioData = () => {
+      setDuration(audio.current.duration);
+    };
+    const updateAudioTime = () => {
+      setCurrentPosition(audio.current.currentTime);
+    };
+    audio.current.addEventListener("loadedmetadata", setAudioData);
+    audio.current.addEventListener("timeupdate", updateAudioTime);
+    return () => {
+      audio.current.removeEventListener("loadedmetadata", setAudioData);
+      audio.current.removeEventListener("timeupdate", updateAudioTime);
+    };
+  }, []);
+
   return (
     <div className="bg-gray-600 flex items-center justify-center h-screen w-full m-auto">
       <div
         className={`bg-white w-96 p-3 ${
           isPlaying ? "flex flex-col" : "flex flex-row"
-        } justify-between rounded-lg`}
+        } justify-between shadow-2xl shadow-black rounded-lg`}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <img
+          id="img"
           src="https://floridaregionfjmc.org/wp-content/uploads/2022/11/Minion-Minyan3.jpeg"
           alt="Melody"
           className={`rounded-md ${isPlaying ? "w-full" : "h-16"}`}
         />
-        <div className={`w-full flex flex-col ${isPlaying ? "mt-1" : "ml-2 justify-center"}`}>
-          {isPlaying && <div>ubub</div>}
+        <div
+          className={`w-full flex flex-col ${
+            isPlaying ? "mt-1" : "ml-2 justify-center"
+          }`}
+        >
+          {isPlaying && (
+            <Slider
+              min={0}
+              max={duration}
+              value={currentPosition}
+              onChange={onSeek}
+              // tipFormatter={(value) => `${Math.round(value)}s`}
+            />
+          )}
           <div className="flex justify-between items-center w-full">
             <div className="flex flex-col">
               <span className="font-bold">Minion Dave</span>
@@ -40,16 +77,20 @@ export default function MelodyPopup() {
               </span>
             </div>
             <div
-              className={`bg-black text-white flex gap-1 items-center ${isHovered || isPlaying ? "py-1 px-2" : "p-2"} rounded-full h-fit`}
+              className={`bg-black text-white flex gap-1 items-center ${
+                isHovered || isPlaying ? "py-1 px-2" : "p-2"
+              } rounded-full h-fit cursor-pointer play-pause-button`}
               onClick={togglePlay}
             >
               {isPlaying ? (
-                <FaPause className="text-xs" />
+                <FaPause className="text-xs pause-icon" />
               ) : (
-                <FaPlay className="text-xs" />
+                <FaPlay className="text-xs play-icon" />
               )}
               {(isPlaying || isHovered) && (
-                <span className="text-xs font-bold">{isPlaying ? "Pause" : "Play"}</span>
+                <span className="text-xs font-bold">
+                  {isPlaying ? "Pause" : "Play"}
+                </span>
               )}
             </div>
           </div>
