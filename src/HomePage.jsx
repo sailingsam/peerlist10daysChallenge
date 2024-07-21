d429e21d66671aaf3835f0ea36061c186a0575a0
sailingsam <jainsaksham1004@gmail.com>
2024-07-22 04:08:23 +0530

    done with adding ch4 in homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,22 +3,29 @@ import { Link } from "react-router-dom";
 import challenge1Gif from "./challenges/challenge1/1.gif";
 import challenge2Gif from "./challenges/challenge2/2.gif";
 import challenge3Gif from "./challenges/challenge3/3.gif";
+import challenge4Gif from "./challenges/challenge4/4.gif";
 
 const challenges = [
   {
     gif: challenge1Gif,
     description: "UpVote Button",
     challengePage: "Challenge1",
-  },{
+  },
+  {
     gif: challenge2Gif,
     description: "Jelly Tags",
     challengePage: "Challenge2",
-  }
-  ,{
+  },
+  {
     gif: challenge3Gif,
     description: "Melody Popup",
     challengePage: "Challenge3",
-  }
+  },
+  {
+    gif: challenge4Gif,
+    description: "Figma Dev Mode",
+    challengePage: "Challenge4",
+  },
 ];
 
 function HomePage() {
@@ -30,9 +37,7 @@ function HomePage() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
         {challenges.map((challenge, index) => (
           <Link to={`/${index + 1}`} key={index}>
-            <div
-              className="bg-white p-4 rounded-lg shadow-xl hover:shadow-gray-700 flex flex-col items-center"
-            >
+            <div className="bg-white p-4 rounded-lg shadow-xl hover:shadow-gray-700 flex flex-col items-center">
               <img
                 src={challenge.gif}
                 alt={challenge.challengePage}
